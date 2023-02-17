d507695edc1fd7874179e4efe90cc547a14ddfae
Nuria Sanchez <nuriasanchezjordan@gmail.com>
2023-02-17 19:00:23 +0100

    versión comentada

--- geometria/src/Rectangulo_NSJ.java
+++ geometria/src/Rectangulo_NSJ.java
@@ -1,19 +1,33 @@
-
+/**
+ * 
+ * @author Nuria Sánchez Jordán
+ * @version 1.2
+ *
+ */
 public class Rectangulo_NSJ extends FiguraGeometrica_NSJ {
 	private double l1;
 	private double l2;
-	
+	/**
+	 * Constructor
+	 * @param tipoFigura forma
+	 * @param lG lado G
+	 * @param lP lado P
+	 */
 	public Rectangulo_NSJ(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
 	}
-
+	/**
+	 * Mide el área
+	 */
 	@Override
 	public double area() {
 		return l1 * l2;
 	}
-	
+	/**
+	 * Mide el perímetro
+	 */
 	@Override
 	public double perimetro() {
 		return (2 * l1) + (2 * l2);
