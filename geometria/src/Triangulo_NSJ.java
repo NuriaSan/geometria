c6c1d4294cb145ab5d3a8417b3c7211947e93f58
Nuria Sanchez <nuriasanchezjordan@gmail.com>
2023-02-17 18:33:02 +0100

    versión refactorizada

---
+++ geometria/src/Triangulo_NSJ.java
@@ -0,0 +1,26 @@
+
+public class Triangulo_NSJ extends FiguraGeometrica_NSJ {
+	private double lado1;
+	private double lado2;
+	private double lado3;
+	
+	public Triangulo_NSJ(String tipoFigura, double lado1, double lado2, double lado3) {
+		super(tipoFigura);
+	}
+
+	@Override
+	public double perimetro() {
+		return lado1 + lado2 + lado3;
+	}
+
+	@Override
+	public double area() { //Utiliza la formula de Her�n
+		double sp;
+		sp = this.semiPerimetro();
+		return Math.sqrt(sp * (sp-lado1) * (sp-lado2) * (sp-lado3));
+	}
+	
+	public double semiPerimetro() {
+		return this.perimetro() / 2;
+	}
+}
